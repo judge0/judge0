08a101e993e325e3e65f12abf41fa4962767d67a
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-09-06 15:48:47 +0200

    Added nginx configuration

--- api/config/initializers/resque.rb
+++ api/config/initializers/resque.rb
@@ -1,2 +1,2 @@
-uri = URI.parse("redis://#{ENV['JUDGE0API_REDIS_HOST']}:6379")
+uri = URI.parse("redis://#{ENV['REDIS_HOST']}:6379")
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :thread_safe => true)
