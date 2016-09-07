db905aa958ec8433d4fddc9dd56a71e9251ca86e
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-09-07 13:33:48 +0200

    Added default redis host to be localhost

--- api/config/initializers/resque.rb
+++ api/config/initializers/resque.rb
@@ -1,2 +1,2 @@
-uri = URI.parse("redis://#{ENV['REDIS_HOST']}:6379")
+uri = URI.parse("redis://#{ENV['REDIS_HOST'].presence || 'localhost'}:6379")
 Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :thread_safe => true)
