413a2929b191f4a1c695c04ff2f35227f6c1e394
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2016-09-12 02:32:02 +0200

    Added CodeClimate test integrations

--- api/spec/spec_helper.rb
+++ api/spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require 'codeclimate-test-reporter'
+
+CodeClimate::TestReporter.start
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
