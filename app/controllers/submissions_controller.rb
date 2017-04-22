ecb2a0ed102fac4f8c92c4b60421c6d9822bbcf3
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2017-04-22 20:13:46 +0200

    Allow base64 encoded data in requests

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -17,7 +17,7 @@ class SubmissionsController < ApplicationController
   private
 
   def submission_params
-    params.permit(
+    submission_params = params.permit(
       :source_code,
       :language_id,
       :number_of_runs,
@@ -33,5 +33,14 @@ class SubmissionsController < ApplicationController
       :enable_per_process_and_thread_memory_limit,
       :max_file_size
     )
+
+    params.include?(:base64_encode) ? decode_params(submission_params) : submission_params
+  end
+
+  def decode_params(params)
+    params[:source_code] = Base64.decode64(params[:source_code]) if params[:source_code]
+    params[:input] = Base64.decode64(params[:input]) if params[:input]
+    params[:expected_output] = Base64.decode64(params[:expected_output]) if params[:expected_output]
+    params
   end
 end
