d3ccc5c5c2d55020c8d497952aa15a37e3369687
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2018-03-17 18:06:14 +0100

    Added migration for migrating source code text attributes to documents

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -3,17 +3,12 @@
 # Table name: submissions
 #
 #  id                                         :integer          not null, primary key
-#  source_code                                :text
 #  language_id                                :integer
-#  stdin                                      :text
-#  expected_output                            :text
-#  stdout                                     :text
 #  status_id                                  :integer
 #  created_at                                 :datetime
 #  finished_at                                :datetime
 #  time                                       :decimal(, )
 #  memory                                     :integer
-#  stderr                                     :text
 #  token                                      :string
 #  number_of_runs                             :integer
 #  cpu_time_limit                             :decimal(, )
@@ -25,11 +20,16 @@
 #  enable_per_process_and_thread_time_limit   :boolean
 #  enable_per_process_and_thread_memory_limit :boolean
 #  max_file_size                              :integer
-#  compile_output                             :text
 #  exit_code                                  :integer
 #  exit_signal                                :integer
-#  message                                    :text
 #  wall_time                                  :decimal(, )
+#  source_id                                  :integer
+#  stdin_id                                   :integer
+#  stdout_id                                  :integer
+#  stderr_id                                  :integer
+#  expected_output_id                         :integer
+#  compile_output_id                          :integer
+#  message_id                                 :integer
 #
 # Indexes
 #
@@ -37,7 +37,6 @@
 #
 
 class Submission < ApplicationRecord
-  validates :source_code, :language_id, presence: true
   validates :number_of_runs,
             numericality: { greater_than: 0, less_than_or_equal_to: Config::MAX_NUMBER_OF_RUNS }
   validates :cpu_time_limit,
@@ -60,10 +59,18 @@ class Submission < ApplicationRecord
             unless: -> { Config::ALLOW_ENABLE_PER_PROCESS_AND_THREAD_MEMORY_LIMIT }
   validates :max_file_size,
             numericality: { greater_than: 0, less_than_or_equal_to: Config::MAX_MAX_FILE_SIZE }
-  validate :language_existence
 
   before_create :generate_token
   before_validation :set_defaults
+
+  belongs_to :language
+  belongs_to :source, class_name: "Document"
+  belongs_to :stdin, class_name: "Document", optional: true
+  belongs_to :stdout, class_name: "Document", optional: true
+  belongs_to :stderr, class_name: "Document", optional: true
+  belongs_to :expected_output, class_name: "Document", optional: true
+  belongs_to :compile_output, class_name: "Document", optional: true
+  belongs_to :message, class_name: "Document", optional: true
 
   enumeration :status
 
@@ -71,83 +78,7 @@ class Submission < ApplicationRecord
 
   self.per_page = 20
 
-  def source_code
-    return nil if super.nil?
-    @decoded_source_code ||= Base64.decode64(self[:source_code]).force_encoding("UTF-8").encode
-  end
-
-  def source_code=(value)
-    super(value)
-    self[:source_code] = Base64.encode64(self[:source_code]) if self[:source_code]
-  end
-  
-
-  def stdin
-    return nil if super.nil?
-    @decoded_stdin ||= Base64.decode64(self[:stdin]).force_encoding("UTF-8").encode
-  end
-
-  def stdin=(value)
-    super(value)
-    self[:stdin] = Base64.encode64(self[:stdin]) if self[:stdin]
-  end
-  
-
-  def stdout
-    return nil if super.nil?
-    @decoded_stdout ||= Base64.decode64(self[:stdout]).force_encoding("UTF-8").encode
-  end
-  
-  def stdout=(value)
-    super(value)
-    self[:stdout] = Base64.encode64(self[:stdout]) if self[:stdout]
-  end
-  
-
-  def expected_output
-    return nil if super.nil?
-    @decoded_expected_output ||= Base64.decode64(self[:expected_output]).force_encoding("UTF-8").encode
-  end
-
-  def expected_output=(value)
-    super(value)
-    self[:expected_output] = Base64.encode64(self[:expected_output]) if self[:expected_output]
-  end
-  
-
-  def stderr
-    return nil if super.nil?
-    @decoded_stderr ||= Base64.decode64(self[:stderr]).force_encoding("UTF-8").encode
-  end
-  
-  def stderr=(value)
-    super(value)
-    self[:stderr] = Base64.encode64(self[:stderr]) if self[:stderr]
-  end
-  
-
-  def compile_output
-    return nil if super.nil?
-    @decoded_compile_output ||= Base64.decode64(self[:compile_output]).force_encoding("UTF-8").encode
-  end
-
-  def compile_output=(value)
-    super(value)
-    self[:compile_output] = Base64.encode64(self[:compile_output]) if self[:compile_output]
-  end
-
-  
-  def language
-    @language ||= Language.find(language_id)
-  end
-
   private
-
-  def language_existence
-    if language_id && Language.find_by_id(language_id).nil?
-      errors.add(:language_id, "language with id #{language_id} doesn't exist")
-    end
-  end
 
   def generate_token
     begin
