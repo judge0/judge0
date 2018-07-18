bf160b7f403b3cab194be21707c3acc2bc171ea8
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2018-07-18 02:04:06 +0200

    Removed some unused indexes

--- db/migrate/20180506112829_create_test_cases.rb
+++ db/migrate/20180506112829_create_test_cases.rb
@@ -2,11 +2,12 @@ class CreateTestCases < ActiveRecord::Migration[5.2]
   def change
     create_table :test_cases do |t|
       t.string     :uuid,   null: false
-      t.references :input,  null: true
-      t.references :output, null: true
+      t.references :input,  null: true, index: false
+      t.references :output, null: true, index: false
       t.timestamps
     end
 
     add_index :test_cases, :uuid, unique: true
+    add_index :test_cases, [:input_id, :output_id], unique: true
   end
 end
