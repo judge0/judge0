962d7d1dcff051357de492f2d31e996405b1d125
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2019-06-17 14:30:50 +0200

    Add REDIS_PORT, REDIS_PASSWORD and POSTGRES_PORT variables.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,6 @@
-uri = URI.parse("redis://#{ENV['REDIS_HOST'].presence || 'localhost'}:6379")
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :thread_safe => true)
+Resque.redis = Redis.new(
+  host:     ENV["REDIS_HOST"].presence || "localhost",
+  port:     ENV["REDIS_PORT"].presence || 6379,
+  password: ENV["REDIS_PASSWORD"],
+  thread_safe: true
+)
