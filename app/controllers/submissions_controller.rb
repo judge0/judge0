0c03acde2c67ee1c8c3a4ec3ac4fd44c8f3b5f0d
srinivas33 <msrinivasa@edyst.com>
2019-08-29 13:37:05 +0530

    permit metadata param

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -71,7 +71,8 @@ class SubmissionsController < ApplicationController
       :max_processes_and_or_threads,
       :enable_per_process_and_thread_time_limit,
       :enable_per_process_and_thread_memory_limit,
-      :max_file_size
+      :max_file_size,
+      { metadata: [:webhook] },
     )
 
     params[:base64_encoded] == "true" ? decode_params(submission_params) : submission_params
