be55ca656b524c5e99e75bd6d6207c4a078f5689
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2020-01-01 04:32:27 +0100

    Use production as default environment. Use default interval of 0.1.

--- config/initializers/configuration.rb
+++ config/initializers/configuration.rb
@@ -1,4 +1,4 @@
-ENV['INTERVAL'] = ENV['INTERVAL'].presence || '2'
+ENV['INTERVAL'] = ENV['INTERVAL'].presence || '0.1'
 ENV['COUNT'] = ENV['COUNT'].presence || '1'
 ENV['QUEUE'] = ENV['QUEUE'].presence || '*'
-ENV['RAILS_ENV'] = ENV['RAILS_ENV'].presence || 'development'
+ENV['RAILS_ENV'] = ENV['RAILS_ENV'].presence || 'production'
