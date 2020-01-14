99939b89a58d21fc7c6a43fcec90550f75681e39
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2020-01-14 16:25:17 +0100

    Don't show total number of workers.

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -8,21 +8,18 @@ class HealthController < ApplicationController
     end
 
     json = []
-    status = :ok
     Resque.queues.each do |queue|
       workers = queues[queue]
       json << {
         queue: queue,
-        total: ENV["COUNT"].to_i,
         available: workers.count,
         idle: workers.count { |w| w.idle? },
         working: workers.count { |w| w.working? },
         paused: workers.count { |w| w.paused? },
         failed: workers.sum { |w| w.failed }
       }
-      status = :internal_server_error if ENV["COUNT"].to_i != workers.count
     end
 
-    render json: json, status: status
+    render json: json
   end
 end
