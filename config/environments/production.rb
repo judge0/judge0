d0630618af8c71eb42036b160e3e5b6bfe0b92e4
Gaurav Kapatia <gkapatia@gmail.com>
2022-03-09 18:47:30 +0530

    setting up of production cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,5 @@
+require 'active_support/core_ext/numeric/bytes'
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -42,7 +44,8 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :memory_store, { size: 200.megabytes }
+
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
