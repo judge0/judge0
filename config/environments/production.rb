f02d4d10acaed71f2e0bb89b0625bddbaa49450f
Gaurav Kapatia <gkapatia@gmail.com>
2022-03-10 14:00:01 +0530

    input-output-from-url-inside-job-redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :mem_cache_store, ENV["MEMCACHED_DOMAIN"]
+  config.cache_store = :redis_cache_store, { url: ENV["REDIS_SIDECAR_SERVER"] }
 
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
