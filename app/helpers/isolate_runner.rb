85bd04d353276afc9b5e85e0ab8429250fa54b66
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2022-05-28 17:53:37 +0200

    Simulate wait=true delegating it to perform_later.

--- app/helpers/isolate_runner.rb
+++ app/helpers/isolate_runner.rb
@@ -1,7 +1,35 @@
 module IsolateRunner
+  MAX_WAIT_TIME_S = 600
+
+  INITIAL_WAIT_TIME_S = 2
+  NEXT_WAIT_TIME_S = 1
+  WAIT_TIME_FACTOR_S = 0.5
+
+  WAITING_STATUSES = [Status.queue.id, Status.process.id, nil]
+
   def self.perform_now(submission)
-    submission.update(status: Status.queue, queued_at: DateTime.now)
-    IsolateJob.perform_now(submission.id)
+    IsolateRunner.perform_later(submission)
+
+    submission_id = submission.id
+
+    total_wait_time = 0
+    (0..).each do |i|
+      break if total_wait_time >= MAX_WAIT_TIME_S
+
+      if i == 0 then
+        wait_time = INITIAL_WAIT_TIME_S
+      elsif i == 1 then
+        wait_time = NEXT_WAIT_TIME_S
+      else
+        wait_time = WAIT_TIME_FACTOR_S * i
+      end
+
+      sleep(wait_time)
+
+      total_wait_time += wait_time
+
+      break if !WAITING_STATUSES.include?(Submission.where(id: submission_id).pluck(:status_id).first)
+    end
   end
 
   def self.perform_later(submission)
