a366f31b517d74751db3d04adb18365941442dbe
Aditya Kumar <adityakumar@Adityas-MacBook-Pro-2.local>
2025-06-10 20:39:18 +0530

    update: language_file

--- db/languages/active.rb
+++ db/languages/active.rb
@@ -376,6 +376,6 @@
     name: "MIPS (Mars 4.5)",
     is_archived: false,
     source_file: "main.s",
-    run_cmd: "/usr/bin/java -jar /usr/local/mars/mars.jar nc main.s < stdin.txt"
+    run_cmd: "/usr/local/bin/java -jar /usr/local/mars/mars.jar nc main.s"
   }
 ]
