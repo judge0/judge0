fd25eded86ecb6bb36380346d86140e1475b904f
Roger Heykoop <roger@heykoop.nl>
2025-12-19 16:54:22 +0100

    Refactor Dockerfile to use compilers base image
    - Use rogerheykoop/compilers:2.0.0 as base image (pending merge to judge0/compilers)
    - Remove embedded compiler installations (now in compilers image)
    - Keep only Rails application setup (Ruby 3.4.1, bundler, gems)
    - Update db/schema.rb to Rails 8.0 format
    
    This restores the proper separation of concerns:
    - compilers image: All language compilers/interpreters + isolate sandbox
    - judge0 image: Rails application layer only
    
    The Dockerfile is now 75 lines instead of 373 lines.

--- db/schema.rb
+++ db/schema.rb
@@ -2,18 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_28_155848) do
-
+ActiveRecord::Schema[8.0].define(version: 2022_05_28_155848) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "clients", id: :string, force: :cascade do |t|
   end
@@ -67,5 +66,4 @@ ActiveRecord::Schema.define(version: 2022_05_28_155848) do
     t.string "execution_host"
     t.index ["token"], name: "index_submissions_on_token"
   end
-
 end
